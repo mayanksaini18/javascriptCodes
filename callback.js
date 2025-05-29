56052ee31c1fb6d653ed6c48afe1f7b1e4ad23e8
Mayank Saini <mayanksaini0416@gmail.com>
2025-05-29 11:37:26 +0530

    call back

---
+++ callback.js
@@ -0,0 +1,16 @@
+function medician1get(){
+    console.log ("medician 1 ")
+    setTimeout(medician2get, 3000)
+    setTimeout(medician3get, 2000)
+
+}
+function medician2get(){
+    console.log ("medician 2 ")
+}
+function medician3get(){
+    console.log ("medician 3 ")
+}
+
+
+medician1get();
+ 
