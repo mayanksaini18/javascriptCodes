11fa9e32c9d40410717cf1f670fb8d6416d20d6e
Mayank Saini <mayanksaini0416@gmail.com>
2025-08-18 13:53:26 +0530

    Create date().js

---
+++ NitinSirclassCodes/ObjectInJs/date().js
@@ -0,0 +1,7 @@
+var now = new Date();
+console.log(now);
+console.log(now.getDate());
+console.log(now.getDay());
+var month = ["JAN" , "FEB" , "MAR" , "APR" ,"JUN" ,"JUL","AUG",]
+console.log(now.getMonth()+1);
+console.log(now.getFullYear())
